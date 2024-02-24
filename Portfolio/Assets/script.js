5aa57e034ab47a8bdb158b8092126a84a9562a5e
izzyjosh <calebayuba222@gmail.com>
2024-02-24 01:32:11 +0100

    Completed skill

--- Portfolio/Assets/script.js
+++ Portfolio/Assets/script.js
@@ -1,4 +1,6 @@
 const toogleBar = document.querySelector(".sidebar")
+const circularProgress = document.querySelectorAll(".circular-progress")
+const progressBar = document.querySelectorAll(".progress-bar")
 
 function showSideBar() {
  toogleBar.style.display = "flex";
@@ -6,4 +8,22 @@ function showSideBar() {
 
 function removeSideBar() {
  toogleBar.style.display = "none";
+}
+
+
+let progressStart = 0
+let speed = 100
+for (let i = 0; i < progressBar.length; i++) {
+
+
+ let progress = setInterval(() => {
+  progressStart++
+  let progressEnd = progressBar[i].getAttribute("data-value")
+  progressBar[i].textContent = `${progressStart}%`
+  circularProgress[i].style.background = `conic-gradient(#bd9106 ${progressStart * 3.6}deg, #000 0deg)`
+  if (progressStart >= progressEnd) {
+   clearInterval(progress)
+  }
+ },
+  speed)
 }
